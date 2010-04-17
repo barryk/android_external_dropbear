4d4ce091a409c15215fcd1bbb7db607be28063b1
Kevin Barry <barryk@gmail.com>
2010-04-17 00:49:45 -0500

    Add dropbear to makefile.
    Used parts of
    http://github.com/cyanogen/android_external_dropbear/commit/df7f9f955b024b0204d57acd635ebbeeb2fd9f05
    
    Change-Id: I36f2510b66956653d1c550bb67ded3b28d7fe41e

--- Android.mk
+++ Android.mk
@@ -50,7 +50,67 @@ LOCAL_CFLAGS += -DDROPBEAR_CLIENT -DPROGRESS_METER
 
 include $(BUILD_EXECUTABLE)
 
-endif  # TARGET_SIMULATOR != true
+include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES:=\
+	dbutil.c buffer.c \
+	dss.c bignum.c \
+	signkey.c rsa.c random.c \
+	queue.c \
+	atomicio.c compat.c fake-rfc2553.c
+
+LOCAL_SRC_FILES+=\
+	common-session.c packet.c common-algo.c common-kex.c \
+	common-channel.c common-chansession.c termcodes.c \
+	tcp-accept.c listener.c process-packet.c \
+	common-runopts.c circbuffer.c \
+	loginrec.c
+
+LOCAL_SRC_FILES+=\
+	svr-kex.c svr-algo.c svr-auth.c sshpty.c \
+	svr-authpasswd.c svr-authpubkey.c svr-authpubkeyoptions.c svr-session.c svr-service.c \
+	svr-chansession.c svr-runopts.c svr-agentfwd.c svr-main.c svr-x11fwd.c \
+	svr-tcpfwd.c svr-authpam.c
+
+
+LOCAL_STATIC_LIBRARIES := libtommath libtomcrypt
+LOCAL_SHARED_LIBRARIES := \
+	libutils \
+	libcutils \
+	libc
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := dropbear
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libtommath 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libtomcrypt/src/headers
+LOCAL_CFLAGS += -DDROPBEAR_SERVER -DANDROID_CHANGES
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=\
+	dbutil.c buffer.c \
+	dss.c bignum.c \
+	signkey.c rsa.c random.c \
+	queue.c \
+	atomicio.c compat.c fake-rfc2553.c
+
+LOCAL_SRC_FILES+=\
+	dropbearkey.c gendss.c genrsa.c
+
+LOCAL_STATIC_LIBRARIES := libtommath libtomcrypt
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE := dropbearkey
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libtommath 
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libtomcrypt/src/headers
+LOCAL_CFLAGS += -DDROPBEAR_SERVER
+
+include $(BUILD_EXECUTABLE)
+
+endif  # TARGET_SIMULATOR != true
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
